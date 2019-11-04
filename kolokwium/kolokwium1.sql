b0b4bc6f059197a28c4b49e9970f7dbef26f0644
asledz <anita.gaja.sledz@gmail.com>
2019-11-04 13:24:32 +0100

    Zadanie 5

--- kolokwium/kolokwium1.sql
+++ kolokwium/kolokwium1.sql
@@ -60,3 +60,19 @@ FROM   (SELECT z.projekt,
         HAVING Nvl(Min(zadanka.procent), 100) = 100)
 WHERE  procent < 100
 GROUP  BY projekt;  
+
+
+-- Zadanie 5
+
+SELECT nazwa
+FROM   (SELECT nazwa,
+               LEVEL poziom
+        FROM   (SELECT z.nazwa,
+                       zal.co
+                FROM   zadanie z
+                       left join zalezy zal
+                              ON z.nazwa = zal.od)
+        CONNECT BY PRIOR nazwa = co)
+GROUP  BY nazwa
+ORDER  BY Max(poziom) ASC,
+          nazwa;  
