63368b9fa7d10abe32545e4e268f8d46afccf02d
asledz <anita.gaja.sledz@gmail.com>
2019-11-04 12:22:50 +0100

    Pierwsze zadanie

--- kolokwium/kolokwium1.sql
+++ kolokwium/kolokwium1.sql
@@ -2,3 +2,13 @@
 
 set linesize 100
 
+
+-- Zadanie 1.
+SELECT z.nazwa,
+       Count(zal.od) zaleznosc
+FROM   zadanie z
+       LEFT JOIN zalezy zal
+              ON z.nazwa = zal.co
+GROUP  BY z.nazwa
+ORDER  BY zaleznosc DESC,
+          z.nazwa;  
