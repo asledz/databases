7ad89e2fd580719fcbcd078c9b4f92fb267e94c3
asledz <anita.gaja.sledz@gmail.com>
2019-11-04 12:38:29 +0100

    Rozwiązanie 2, chyba ok?

--- kolokwium/kolokwium1.sql
+++ kolokwium/kolokwium1.sql
@@ -12,3 +12,22 @@ FROM   zadanie z
 GROUP  BY z.nazwa
 ORDER  BY zaleznosc DESC,
           z.nazwa;  
+
+
+-- Zadanie 2. 
+
+SELECT z.nazwa--,
+--       zal.od,
+--       Sum(a1.iletrwa)           osobodni,
+--       ( z.koniec - z.poczatek ) trwa
+FROM   zadanie z
+       LEFT JOIN zalezy zal
+              ON z.nazwa = zal.co
+       LEFT JOIN (SELECT z.nazwa,
+                         ( z.koniec - z.poczatek ) * z.osoby iletrwa
+                  FROM   zadanie z) a1
+              ON a1.nazwa = zal.od
+GROUP  BY z.nazwa,
+          zal.od,
+          ( z.koniec - z.poczatek )
+HAVING Sum(a1.iletrwa) < ( z.koniec - z.poczatek );  
