51d31d6560674732334199c3aa3159409fdb38ca
asledz <anita.gaja.sledz@gmail.com>
2019-11-04 13:00:27 +0100

    Poprawione rozwiazanie 2.

--- kolokwium/kolokwium1.sql
+++ kolokwium/kolokwium1.sql
@@ -13,26 +13,18 @@ GROUP  BY z.nazwa
 ORDER  BY zaleznosc DESC,
           z.nazwa;  
 
-
--- Zadanie 2. 
-
-SELECT z.nazwa--,
---       zal.od,
---       Sum(a1.iletrwa)           osobodni,
---       ( z.koniec - z.poczatek ) trwa
-FROM   zadanie z
-       LEFT JOIN zalezy zal
-              ON z.nazwa = zal.co
-       LEFT JOIN (SELECT z.nazwa,
-                         ( z.koniec - z.poczatek ) * z.osoby iletrwa
-                  FROM   zadanie z) a1
-              ON a1.nazwa = zal.od
-GROUP  BY z.nazwa,
-          zal.od,
-          ( z.koniec - z.poczatek )
-HAVING Sum(a1.iletrwa) < ( z.koniec - z.poczatek );  
-
-
+-- Zadanie 2.
+SELECT a1.projekt
+FROM   (SELECT z.projekt,
+               Max(z.koniec) - Min(z.poczatek) iletrwa
+        FROM   zadanie z
+        GROUP  BY z.projekt) a1
+       JOIN (SELECT z.projekt,
+                    Sum(( z.koniec - z.poczatek ) * z.osoby) lrg
+             FROM   zadanie z
+             GROUP  BY z.projekt) a2
+         ON a1.projekt = a2.projekt
+WHERE  iletrwa > lrg;  
 
 -- Zadanie 3.
 -- Chyba źle!!!
@@ -47,3 +39,17 @@ FROM   zadanie z
        LEFT JOIN zadanie zadanka
               ON zal.od = zadanka.nazwa
 GROUP  BY z.nazwa;  
+
+
+SELECT z.*, zal.co kolejny
+FROM
+zadanie z
+LEFT JOIN
+zalezy zal
+ON zal.od = z.nazwa;
+
+
+SELECT CONCAT(LPAD(' ',LEVEL-1),ename)
+FROM emp
+START WITH mgr IS NULL
+CONNECT BY PRIOR empno=mgr;
